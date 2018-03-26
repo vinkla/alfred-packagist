6d468be2cd584a58c714dad0855e3e0f6d2394b4
Vincent Klaiber <vincentklaiber@gmail.com>
2018-03-26 17:42:06 +0200

    Update package with async + await

--- index.js
+++ index.js
@@ -2,26 +2,27 @@
 
 const alfy = require('alfy');
 
-alfy.fetch('https://packagist.org/search.json', {
-  query: {
-    q: alfy.input
-  }
-}).then(data => {
-  const items = data.results
-    .map(x => {
-      return {
-        title: x.name,
-        subtitle: x.description,
-        arg: x.repository || x.url,
-        mods: {
-          alt: {
-            arg: x.url,
-            subtitle: 'Open the Packagist page instead of the GitHub repo'
-          }
-        },
-        quicklookurl: x.repository && `${x.repository}#readme`
-      };
-    });
+(async () => {
+  const data = await alfy.fetch('https://packagist.org/search.json', {
+    query: {
+      q: alfy.input
+    }
+  });
+
+  const items = data.results.map(x => {
+    return {
+      title: x.name,
+      subtitle: x.description,
+      arg: x.repository || x.url,
+      mods: {
+        alt: {
+          arg: x.url,
+          subtitle: 'Open the Packagist page instead of the GitHub repo'
+        }
+      },
+      quicklookurl: x.repository && `${x.repository}#readme`
+    };
+  });
 
   alfy.output(items);
-});
+})();
